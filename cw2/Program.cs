85d3a7d1259c4be3da724b9400d46db706d0d9bf
Klaudia Jakubowska <s19520@pjwstk.edu.pl>
2020-11-18 23:42:59 +0100

    cw2

---
+++ cw2/Program.cs
@@ -0,0 +1,76 @@
+﻿using CsvHelper;
+using cw2.models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zrodlo = "data.csv", result = "result.xml", format = "xml";
+           
+            
+
+            if (args.Length >= 1)
+            {
+                    zrodlo = args[0];
+            }
+            if (args.Length >= 2)
+            {
+                result = args[1];
+            }
+            if (args.Length >= 3)
+            {
+                format = args[2];
+            }
+            try
+            {
+                using (StreamReader reader = new StreamReader(zrodlo))
+
+
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    Student student = new Student();
+                    csv.Configuration.HasHeaderRecord = false;
+                    List<Student> records = csv.GetRecords<Student>().ToList();
+
+                    FileStream writer = new FileStream(result, FileMode.Create);
+                    if (format.Equals("xml"))
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+                        serializer.Serialize(writer, records);
+                        writer.Close();
+                    }
+                    if (format.Equals("json"))
+                    {
+                        string jsonString = JsonSerializer.Serialize(records);
+                        File.WriteAllText("data.json", jsonString);
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                using (FileStream fs = File.Create("log.txt"))
+                {
+                    AddText(fs, "FileNotFoundException: Plik nazwa nie istnieje\r");
+                    AddText(fs, e.Message);
+                 
+                }
+            }
+        }
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+    }
+}
---
+++ cw2/models/Student.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace cw2.models
+{
+    [Serializable]
+    public class Student
+    {
+        [Index(0)]
+        [JsonPropertyName("Imie")]
+        public string fname { get; set; }
+        [Index(1)]
+        
+        public string lname { get; set; }
+        [Index(2)]
+        public string studies { get; set; }
+        [Index(3)]
+        public string mode { get; set; }
+        [Index(4)]
+        [XmlAttribute("indexName")]
+        public string IndexNumber { get; set; }
+        [Index(5)]
+        public DateTime birthdate { get; set; }
+        [Index(6)]
+        public string mothersName { get; set; }
+        [Index(7)]
+        public string fathersName { get; set; }
+    }
+    
+    
+}
